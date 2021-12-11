64d3d5c263916b2ef311e63aed58537d5fe876f4
OdinhengeT <onshage.torbjorn@gmail.com>
2021-12-11 13:49:08 +0100

    added testing for collisionHandler

--- testing/testMain.cpp
+++ testing/testMain.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <memory>
+#include <chrono>
+#include <thread>
 
 #include "../src/entity/entity.hpp"
 #include "../src/graphics/frame.hpp"
@@ -8,21 +10,55 @@
 int main() {
     std::cout << "Creating Window" << std::endl;
 
+    static const float TIME_STEP = 1.0f / 60.0f; // 1 / fps (delta time in seconds)
+
     auto pWindow = std::unique_ptr<Window>(new Window(L"Pong", 1440, 900));
 
     Frame* frame = new Frame(1440, 900);
- 
-    frame->drawString(50, 50, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!=", 4, 6, makeRGB(255, 128, 0));
+
+    Entity one(200, 200, 80, 80, 10);
+    Entity two(400, 200, 80, 80, 10);
+    one.setVelocityX(10);
+
 
     bool running = true; 
     while (running) {
+        
+        auto t0 = std::chrono::high_resolution_clock::now();
+        auto t1 = t0 + std::chrono::microseconds(static_cast<int>(1000000.0f * TIME_STEP));
 
         if (!pWindow->MsgProc()) {
             std::cout << "Closing Window" << std::endl;
             running = false;
         }
 
+        frame->clear();
+
+        frame->drawString(50, 50, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!=", 4, 6, makeRGB(255, 128, 0));
+
+        frame->drawRectangle(
+            static_cast<int>(one.getPositionX() + 0.5f), 
+            static_cast<int>(one.getPositionY() + 0.5f), 
+            static_cast<int>(one.getWidth() + 0.5f), 
+            static_cast<int>(one.getHeight()) + 0.5f, 
+            makeRGB(255, 255, 255)
+        );
+        frame->drawRectangle(
+            static_cast<int>(two.getPositionX() + 0.5f), 
+            static_cast<int>(two.getPositionY() + 0.5f), 
+            static_cast<int>(two.getWidth() + 0.5f), 
+            static_cast<int>(two.getHeight()) + 0.5f, 
+            makeRGB(255, 255, 255)
+        );
+
+        one.timeStep(TIME_STEP);        
+        two.timeStep(TIME_STEP);
+
+        collisionHandler(one, two);
+
         pWindow->drawFrame(frame->getBitmap_info(), frame->getPixels());
+
+        std::this_thread::sleep_until(t1);
 
     }
 
